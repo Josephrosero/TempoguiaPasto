f06fcccf75cf9ce9a58b045dcabe7ff10c260b7f
Josephrosero <josephrosero47@gmail.com>
2018-05-15 16:43:32 -0500

    logo-mapa-ubicacionusurio

--- app/src/main/java/com/example/asus/tempoguiapasto/MapboxNavigation.java
+++ app/src/main/java/com/example/asus/tempoguiapasto/MapboxNavigation.java
@@ -69,6 +69,9 @@ public class MapboxNavigation extends AppCompatActivity implements LocationEngin
 
     @SuppressWarnings( {"MissingPermission"})
     private void initializeLocationEngine() {
+
+
+        ///////
         locationEngine = new LostLocationEngine(MapboxNavigation.this);
         locationEngine.setPriority(LocationEnginePriority.HIGH_ACCURACY);
         locationEngine.activate();
