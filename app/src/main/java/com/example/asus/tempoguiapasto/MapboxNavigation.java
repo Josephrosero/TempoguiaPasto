ecd5d20d7f0ceddc59c24fdc01432a5361f5785c
Josephrosero <josephrosero47@gmail.com>
2018-05-17 09:52:39 -0500

    logo-mapa-ubicacionusurio-sicompila

--- app/src/main/java/com/example/asus/tempoguiapasto/MapboxNavigation.java
+++ app/src/main/java/com/example/asus/tempoguiapasto/MapboxNavigation.java
@@ -1,30 +1,33 @@
 package com.example.asus.tempoguiapasto;
 
+import android.graphics.BitmapFactory;
 import android.location.Location;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 
-import com.mapbox.android.core.location.LocationEngine;
-import com.mapbox.android.core.location.LocationEngineListener;
-import com.mapbox.android.core.location.LocationEnginePriority;
-import com.mapbox.android.core.permissions.PermissionsListener;
-import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.android.core.location.LocationEngineProvider;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
-import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerMode;
-import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
-import com.mapbox.services.android.location.LostLocationEngine;
 
+import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerOptions;
+import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
+
+import com.mapbox.services.android.navigation.ui.v5.NavigationActivity;
+import com.mapbox.android.core.location.LocationEngine;
+import com.mapbox.android.core.location.LocationEngineListener;
+import com.mapbox.android.core.location.LocationEnginePriority;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
 import java.util.List;
 
 
-public class MapboxNavigation extends AppCompatActivity implements LocationEngineListener,PermissionsListener {
+public class MapboxNavigation extends AppCompatActivity implements LocationEngineListener, PermissionsListener {
 
      private MapView mapView;
     private MapboxMap map;
@@ -38,20 +41,28 @@ public class MapboxNavigation extends AppCompatActivity implements LocationEngin
         Mapbox.getInstance(this, getString(R.string.access_token));
         setContentView(R.layout.activity_mapbox_navigation);
         mapView = (MapView) findViewById(R.id.mapView);
-        mapView.onCreate(savedInstanceState);
 
+        mapView.onCreate(savedInstanceState);
 
         mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(final MapboxMap mapboxMap) {
-        map = mapboxMap;
-        enableLocationPlugin();
- }
-      ;
+
+
+                map = mapboxMap;
+                enableLocationPlugin();
+
+
+            }
+
+            ;
         });
 
-
     }
+
+    /////ubicacion del usuario
+
+
     @SuppressWarnings( {"MissingPermission"})
     private void enableLocationPlugin() {
         // Check if permissions are enabled and if not request
@@ -59,20 +70,20 @@ public class MapboxNavigation extends AppCompatActivity implements LocationEngin
             // Create an instance of LOST location engine
             initializeLocationEngine();
 
-            locationPlugin = new LocationLayerPlugin(mapView, map, locationEngine);
-            locationPlugin.setLocationLayerEnabled(LocationLayerMode.TRACKING);
+            locationPlugin = new LocationLayerPlugin(mapView, map,locationEngine);
+
+            locationPlugin.setLocationLayerEnabled(true);
+
         } else {
             permissionsManager = new PermissionsManager(this);
             permissionsManager.requestLocationPermissions(this);
         }
     }
-
     @SuppressWarnings( {"MissingPermission"})
     private void initializeLocationEngine() {
 
+        locationEngine = new LocationEngineProvider(MapboxNavigation.this).obtainBestLocationEngineAvailable() ;
 
-        ///////
-        locationEngine = new LostLocationEngine(MapboxNavigation.this);
         locationEngine.setPriority(LocationEnginePriority.HIGH_ACCURACY);
         locationEngine.activate();
 
@@ -84,6 +95,7 @@ public class MapboxNavigation extends AppCompatActivity implements LocationEngin
             locationEngine.addLocationEngineListener(this);
         }
     }
+
 
     private void setCameraPosition(Location location) {
         map.animateCamera(CameraUpdateFactory.newLatLngZoom(
@@ -124,8 +136,6 @@ public class MapboxNavigation extends AppCompatActivity implements LocationEngin
         }
     }
 
-    //////////////////////////////////////
-
     @Override
     @SuppressWarnings( {"MissingPermission"})
     protected void onStart() {
@@ -161,21 +171,21 @@ public class MapboxNavigation extends AppCompatActivity implements LocationEngin
     }
 
     @Override
-    public void onResume() {
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onResume() {
         super.onResume();
         mapView.onResume();
     }
 
     @Override
-    public void onPause() {
+    protected void onPause() {
         super.onPause();
         mapView.onPause();
-    }
-
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mapView.onLowMemory();
     }
 
     @Override
@@ -183,5 +193,7 @@ public class MapboxNavigation extends AppCompatActivity implements LocationEngin
         super.onSaveInstanceState(outState);
         mapView.onSaveInstanceState(outState);
     }
+
+
 
 }
